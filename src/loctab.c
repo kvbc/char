667bb27d6b244c31a2f2e9d8ae94a91118a3887a
kvbc <64214044+kvbc@users.noreply.github.com>
2020-10-23 13:25:52 +0200

    Update loctab.c

--- src/loctab.c
+++ src/loctab.c
@@ -14,7 +14,7 @@
 
 char_loctab_t* char_loctab_new(uint32_t cap) {
 	char_loctab_t* lt = malloc(sizeof(char_loctab_t));
-	lt->cur = 0;
+	lt->top = 0;
 	lt->cap = cap;
 	lt->mem = malloc(sizeof(char_loc_t) * cap);
 	return lt;
@@ -22,19 +22,19 @@ char_loctab_t* char_loctab_new(uint32_t cap) {
 
 
 void char_loctab_new_entry(char_loctab_t* lt, char* id, uint32_t idx, uint32_t col, uint32_t line) {
-	if(lt->cur == lt->cap)
+	if(lt->top == lt->cap)
 		lt->mem = realloc(lt->mem, sizeof(char_loc_t) * (lt->cap += CHAR_LOCTAB_CAP_INC));
 	char_loc_t l;
 	l.idx = idx;
 	l.col = col;
 	l.line = line;
 	l.id = char_str_set(char_str_new(), id, strlen(id));
-	lt->mem[lt->cur++] = l;
+	lt->mem[lt->top++] = l;
 }
 
 
 char_loc_t* char_loctab_lookup(char_loctab_t* lt, char* id) {
-	for(uint32_t i = 0; i < lt->cur; ++i)
+	for(uint32_t i = 0; i < lt->top; ++i)
 		if(strcmp(lt->mem[i].id->src, id) == 0)
 			return &lt->mem[i];
 	return NULL;
@@ -42,7 +42,7 @@ char_loc_t* char_loctab_lookup(char_loctab_t* lt, char* id) {
 
 
 void char_loctab_free(char_loctab_t* lt) {
-	for(uint32_t i = 0; i < lt->cur; ++i)
+	for(uint32_t i = 0; i < lt->top; ++i)
 		char_str_free(lt->mem[i].id);
 	free(lt->mem);
 	free(lt);
