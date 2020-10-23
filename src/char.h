dd15bd17df1d0e9999ad82c32d90d17b6715a7be
kvbc <64214044+kvbc@users.noreply.github.com>
2020-10-23 13:16:17 +0200

    tabs

--- src/char.h
+++ src/char.h
@@ -9,15 +9,15 @@
 #pragma once
 #include <stdbool.h>
 
-#define CHAR_STATE					bool
-#define CHAR_ERROR					false
-#define CHAR_SUCCESS				true
+#define CHAR_STATE				bool
+#define CHAR_ERROR				false
+#define CHAR_SUCCESS			true
 
-#define CHAR_DEF_INPUT				""			// Default input of a program
+#define CHAR_DEF_INPUT			""			// Default input of a program
 
-#define CHAR_STACK_CAP_INC			50			// Capacity increase of a stack
-#define CHAR_LOCTAB_CAP_INC			50			// Capacity increase of a location table
+#define CHAR_STACK_CAP_INC		50			// Capacity increase of a stack
+#define CHAR_LOCTAB_CAP_INC		50			// Capacity increase of a location table
 
-#define CHAR_VM_STACK_SIZE			100			// Default stack size for the VM
-#define CHAR_VM_RETS_SIZE			50			// Default size of the stack for "returns" for the VM
-#define CHAR_VM_LBLS_SIZE			50			// Default size of the location table for labels for the VM
+#define CHAR_VM_STACK_SIZE		100			// Default stack size for the VM
+#define CHAR_VM_RETS_SIZE		50			// Default size of the stack for "returns" for the VM
+#define CHAR_VM_LBLS_SIZE		50			// Default size of the location table for labels for the VM
