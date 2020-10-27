b9bcb35df4a8e571789a682d135bfa6a5d049fd4
kvbc <64214044+kvbc@users.noreply.github.com>
2020-10-27 11:38:31 +0100

    Update err.c

--- src/err.c
+++ src/err.c
@@ -11,22 +11,22 @@
 #include <stdarg.h>
 
 
-static void char_err_vprint(char* fn, uint32_t line, uint32_t col, char* frmt, va_list* args) {
-	printf("%s:%d:%d: ", fn, line, col);
+static void char_err_vprint(char* type, uint32_t line, uint32_t col, char* frmt, va_list* args) {
+	printf("%sError :%d:%d: ", type, line, col);
 	vprintf(frmt, *args);
 	va_end(*args);
 }
 
 
-void char_err_print(char* fn, uint32_t line, uint32_t col, char* frmt, ...) {
+void char_err_print(char* type, uint32_t line, uint32_t col, char* frmt, ...) {
 	va_list args;
 	va_start(args, frmt);
-	char_err_vprint(fn, line, col, frmt, &args);
+	char_err_vprint(type, line, col, frmt, &args);
 }
 
 
-void char_err_rdrprint(char* fn, char_reader_t* rdr, uint32_t offset, char* frmt, ...) {
+void char_err_rdrprint(char* type, char_reader_t* rdr, uint32_t offset, char* frmt, ...) {
 	va_list args;
 	va_start(args, frmt);
-	char_err_vprint(fn, rdr->line, rdr->col - offset, frmt, &args);
+	char_err_vprint(type, rdr->line, rdr->col - offset, frmt, &args);
 }
